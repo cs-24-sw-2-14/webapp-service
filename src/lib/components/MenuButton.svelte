77de03a79e6e16821a0568a0cd57296850ef6116
Marc Nygaard <marcnygaard100@gmail.com>
2024-03-31 11:45:11 +0200

    Button active color fix

--- src/lib/components/MenuButton.svelte
+++ src/lib/components/MenuButton.svelte
@@ -3,11 +3,19 @@
 	export let isActive: boolean = false;
 </script>
 
-<button
-	on:click
-	class:bg-gray-50={isActive}
-	class:text-black={isActive}
-	class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 text-gray-50 hover:text-black hover:bg-gray-50"
->
-	{@html icon}
-</button>
+<div class:text-black={true}>
+	<button
+		on:click
+		class:bg-gray-50={isActive}
+		class:text-gray-50={!isActive}
+		class="inline-flex items-center justify-center h-10 px-4 py-2 text-sm font-medium text-black transition-colors rounded-md focus-visible:outline-none hover:text-black hover:bg-gray-50"
+	>
+		{@html icon}
+	</button>
+</div>
+
+<style>
+	.icon-fill {
+		fill: black;
+	}
+</style>
--- src/lib/components/Ping.svelte
+++ src/lib/components/Ping.svelte
@@ -1,5 +1,5 @@
 <div
-	class="fixed bottom-0 right-0 mb-16 mr-[76px] rounded-lg bg-zinc-800 z-10 flex items-center gap-2 pointer-events-none"
+	class="fixed bottom-0 right-0 mb-16 mr-[80px] rounded-lg bg-zinc-800 z-10 flex items-center gap-2 pointer-events-none"
 >
 	<span class="relative flex h-3 w-3">
 		<span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"
