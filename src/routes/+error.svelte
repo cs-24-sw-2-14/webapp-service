25fe9bf961879d44ed2776444d3b95f4ae049398
Cecilie Lassen <classe23@student.aau.dk>
2024-04-12 14:59:26 +0200

    Error 404
    An error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+
+</script> 
+
+
+<main>
+<!--h1 and p are text-->
+<h1>ERROR 404</h1>
+<p>Sorry, the page you are looking for could not be found</p>
+<p>Here is a button to the main page. You can click it, if you want to </p>
+<p>¯\_(ツ)_/¯</p>
+<a href="/"> <button class="button">Main Page</button> <!--href="/" is used to link to the main page-->
+</a>
+</main>
+
+
+<style>
+    h1{
+        color: darkblue; /*Changes the color of the text in h1*/
+        font-weight: bold; /**/
+        font-size: 30px; /**/
+    }
+    * {
+  text-align: center; /*Centers all the text*/
+  position:relative; top:40px; /**/
+}
+.button {
+  border-radius: 50%; /*Makes it oval*/
+  background-color: #ccabf6; /*The color of the button*/
+  border: solid blueviolet; /*Makes a border in the color blueviolet*/
+  color: black; /*Makes the text color black*/
+  padding: 16px 16px; /**/
+  display: inline; /**/
+  font-size: 20px; /**/
+  height:   100px;
+  width: 100px;
+}
+
+
+</style>
+
