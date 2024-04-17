7e7fbde95d4b51d37f009d456d3458c42378518c
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-04-17 10:09:17 +0200

    Prettier format

--- src/lib/stores/stateStore.ts
+++ src/lib/stores/stateStore.ts
@@ -9,37 +9,37 @@ export const viewChat = writable(false);
 export const settings = writable(false);
 
 export interface CanvasView {
-	x: number;
-	y: number;
-	width: number; // Default values, will be replaced on mount
-	height: number; // Default values, will be replaced on mount
-	scale: number;
+  x: number;
+  y: number;
+  width: number; // Default values, will be replaced on mount
+  height: number; // Default values, will be replaced on mount
+  scale: number;
 }
 
 export const canvasView = writable<CanvasView>({
-	x: 0,
-	y: 0,
-	width: 0,
-	height: 0,
-	scale: 100
+  x: 0,
+  y: 0,
+  width: 0,
+  height: 0,
+  scale: 100
 });
 
 export interface ToolStates {
-	pencil: boolean;
-	erase: boolean;
-	pan: boolean;
-	move: boolean;
+  draw: boolean;
+  erase: boolean;
+  pan: boolean;
+  move: boolean;
 }
 
 export const toolState = writable<ToolStates>({
-	pencil: false,
-	erase: false,
-	pan: true,
-	move: false
+  draw: false,
+  erase: false,
+  pan: true,
+  move: false
 });
 
 export function activateTool(tool: keyof ToolStates) {
-	toolState.update((current) => {
-		return { ...current, pencil: false, erase: false, pan: false, move: false, [tool]: true };
-	});
+  toolState.update((current) => {
+    return { ...current, draw: false, erase: false, pan: false, move: false, [tool]: true };
+  });
 }
