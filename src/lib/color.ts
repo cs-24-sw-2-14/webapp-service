e23fe0206fdec217833d1eac69d37663cb9d4529
MalleGJ <amalie@gabejensen.dk>
2024-04-18 10:57:30 +0200

    Cleaned up the code a bit and added comments

--- src/lib/color.ts
+++ src/lib/color.ts
@@ -9,22 +9,10 @@ export let colors = [
     createColor("orange","#f97316", "#9a3412"),
     createColor("yellow", "#fbbf24", "#b45309"),
     createColor("lime", "#84cc16", "#3f6212"),
-    createColor("green", "#22c55e", "#166534"),
+    createColor("green", "#15803d", "#14532d"),
     createColor("teal", "#2dd4bf", "#0f766e"),
     createColor("brown", "#713f12", "#451a03"),
     createColor("blue", "#3b82f6", "#1e40af"),
     createColor("purple", "#a855f7", "#6b21a8"),
     createColor("pink", "#ec4899", "#9d174d"),
-/*
-    createColor("red","mainRed","borderRed"),
-    createColor("","","borderOrange"),
-    createColor("","","borderYellow"),
-    createColor("","","borderLime"),
-    createColor("","","borderGreen"),
-    createColor("","","borderTeal"),
-    createColor("","","borderBrown"),
-    createColor("","","borderBlue"),
-    createColor("","","borderPurple"),
-    createColor("","","borderPink"),
-*/
 ]
--- src/lib/components/ColorPicker.svelte
+++ src/lib/components/ColorPicker.svelte
@@ -3,6 +3,7 @@
 	import { colors } from '../color';
 	import type { User, Color } from '$lib/stores/stateStore';
 
+	/* Checks if a color is chosen and returns true or false so the color-button gets disabled */
 	function colorChosen(color: Color, user: User, onlineUsers: User[]) {
 		if (user && user.color && user.color.name === color.name) {
 			return true;
@@ -17,6 +18,7 @@
 		return false;
 	}
 
+	/* Changes the users color to the color they picked */
 	function colorClicked(color: Color) {
 		user.update((current: User) => {
 			current.color = color;
@@ -27,21 +29,22 @@
 	}
 </script>
 
+<!-- Table of all the colors -->
 <div class="container">
 	<table>
 		{#each [0, 1, 2] as row}
 			<tr>
 				{#each colors.slice(row * 5, row * 5 + 5) as color}
-					<td
-						><button
+					<td>
+						<button
 							style={`background-color: ${color.bg}; border-color: ${color.border};`}
 							class={`border-2 hover:border-4`}
 							disabled={colorChosen(color, $user, $onlineUsers)}
 							on:click={() => {
 								colorClicked(color);
-							}}
-						></button></td
-					>
+							}}>
+						</button>
+					</td>
 				{/each}
 			</tr>
 		{/each}
@@ -53,32 +56,32 @@
 		background-color: #ef4444;
 	}
 
-	/* Centering styles */
+	/* Container-div for the table to center it */
 	.container {
 		display: flex;
 		justify-content: center; /* Center horizontally */
 		align-items: center; /* Center vertically */
 	}
 
-	/* Button styles */
+	/* Color-Button styles */
 	button {
 		border-radius: 50%;
 		width: 2.5em;
 		height: 2.5em;
 		vertical-align: middle; /* Align vertically */
-		position: relative; /* Set position to relative */
+		position: relative; 
 		overflow: hidden; /* Allow content to overflow */
 		margin: 0.5em; /* Add margin around the button */
 	}
 
+	/* Color-button when disabled style*/
 	button:disabled::after {
-		content: '✕'; /* Add 'X' content */
-		position: absolute; /* Set position to absolute */
-		top: 45%; /* Align to the center vertically */
-		left: 50%; /* Align to the center horizontally */
+		content: '✕'; /* Add 'X' as button-content */
+		position: absolute;
+		top: 45%; /* Align to the center vertically - usually 50% but the X is not full height and so needs to be raised a bit*/
 		transform: translate(-50%, -50%); /* Center the content */
 		font-size: 300%; /* Increase font size */
 		z-index: 1; /* Ensure X appears above button */
-		color: #27272a; /* Set color to grey */
+		color: #27272a; /* Set color to dark grey */
 	}
 </style>
