081beb20ae94dac6fe95928c6044118f6ad1bd00
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-04-25 12:53:28 +0200

    Refactor svgStore

--- src/lib/stores/svgStore.ts
+++ src/lib/stores/svgStore.ts
@@ -1,16 +1,9 @@
 // svgStore.js
 import { writable } from 'svelte/store';
 
-export interface Svg {
-  svg: string;
-  x: number;
-  y: number;
-}
-
 export enum CoordinateType {
-  start,
-  middle,
-  end,
+  moveto,
+  lineto,
 }
 
 export interface PathCoordinate {
@@ -25,13 +18,40 @@ export interface PlacementCoordinate {
 }
 
 export interface Drawing {
-  path: PathCoordinate[];
-  stroke: string;
-  fill: string;
-  strokeWidth: number;
-  placement: PlacementCoordinate;
-
+  path?: PathCoordinate[];
+  stroke?: string;
+  fill?: string;
+  strokeWidth?: number;
+  placement?: PlacementCoordinate;
 }
 
-export const drawings = writable<Drawing[]>([
-]);
+export enum svgType {
+  drawing,
+}
+
+export function pathToString(path: PathCoordinate[]) {
+  return path.map((coordinate) => {
+    if (coordinate.type === CoordinateType.moveto) {
+      return `M${coordinate.x},${coordinate.y}`
+    } else {
+      return `L${coordinate.x},${coordinate.y}`
+    }
+  }).join("")
+}
+
+export const drawings = writable<Drawing[]>([]);
+
+export function addDrawing(changes: Drawing) {
+  $drawings = [
+    ...$drawings,
+    changes
+  ]
+}
+
+export function modifyDrawing(index: number, changes: Drawing) {
+  $drawings[index] = changes
+}
+
+export function deleteDrawing(index: number) {
+  $drawings.splice(index, index)
+}
