9ad8cba76bae31f3f9ccfdd7d744e11218e26ca0
Kresten Laust <krestenlaust@gmail.com>
2024-04-25 22:15:11 +0200

    Add Canvas element to overlay
    I've added the element and it resizes properly, but it's not
    see-through.

--- src/lib/components/Canvas.svelte
+++ src/lib/components/Canvas.svelte
@@ -20,65 +20,69 @@
 			height: window.innerHeight
 		};
 	}
+
 </script>
 
 <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 <!-- svelte-ignore a11y-mouse-events-have-key-events -->
-<svg
-	class:draggable={$toolState === ToolState.pan}
-	role="application"
-	aria-label="Interactive infinite whiteboard with draggable grid"
-	xmlns="http://www.w3.org/2000/svg"
-	width="100%"
-	height="100%"
-	viewBox={`
-  ${$canvasView.x - $canvasView.width / ((2 * $canvasView.scale) / 100)} 
-  ${$canvasView.y - $canvasView.height / ((2 * $canvasView.scale) / 100)} 
-  ${$canvasView.width / ($canvasView.scale / 100)} 
-  ${$canvasView.height / ($canvasView.scale / 100)}
-`}
-	on:mousedown={mouseEvents.down}
-	on:mousemove={mouseEvents.move}
-	on:mouseup={mouseEvents.up}
-	on:mouseleave={mouseEvents.up}
-	on:resize={resizeCanvas}
->
-	<!-- Define the pattern for the dotted background -->
-	<pattern
-		id="pattern-circles"
-		x="0"
-		y="0"
-		width="50"
-		height="50"
-		patternUnits="userSpaceOnUse"
-		patternContentUnits="userSpaceOnUse"
-	>
-		<circle cx="10" cy="10" r="1.5" fill="#8c8c8c" />
-	</pattern>
-
-	<!-- Render background pattern -->
-	<rect
-		x={$canvasView.x - $canvasView.width / ((2 * $canvasView.scale) / 100)}
-		y={$canvasView.y - $canvasView.height / ((2 * $canvasView.scale) / 100)}
+<div style="width: 100%; height: 100%;">
+	<canvas id="overlaycanvas" style="width: 100%;height: 100%" width="{$canvasView.width}" height="{$canvasView.height}" />
+	<svg
+		class:draggable={$toolState === ToolState.pan}
+		role="application"
+		aria-label="Interactive infinite whiteboard with draggable grid"
+		xmlns="http://www.w3.org/2000/svg"
 		width="100%"
 		height="100%"
-		fill="url(#pattern-circles)"
-	/>
+		viewBox={`
+  ${$canvasView.x - $canvasView.width / ((2 * $canvasView.scale) / 100)}
+  ${$canvasView.y - $canvasView.height / ((2 * $canvasView.scale) / 100)}
+  ${$canvasView.width / ($canvasView.scale / 100)}
+  ${$canvasView.height / ($canvasView.scale / 100)}
+`}
+		on:mousedown={mouseEvents.down}
+		on:mousemove={mouseEvents.move}
+		on:mouseup={mouseEvents.up}
+		on:mouseleave={mouseEvents.up}
+		on:resize={resizeCanvas}
+	>
+		<!-- Define the pattern for the dotted background -->
+		<pattern
+			id="pattern-circles"
+			x="0"
+			y="0"
+			width="50"
+			height="50"
+			patternUnits="userSpaceOnUse"
+			patternContentUnits="userSpaceOnUse"
+		>
+			<circle cx="10" cy="10" r="1.5" fill="#8c8c8c" />
+		</pattern>
 
-	<!-- Render the SVGs -->
-	{#each $svgs as svgObj, index}
-		<!-- svelte-ignore a11y-mouse-events-have-key-events -->
-		<!-- svelte-ignore a11y-no-static-element-interactions -->
-		<g
-			transform={`translate(${svgObj.x}, ${svgObj.y})`}
-			on:mouseover={() => {
+		<!-- Render background pattern -->
+		<rect
+			x={$canvasView.x - $canvasView.width / ((2 * $canvasView.scale) / 100)}
+			y={$canvasView.y - $canvasView.height / ((2 * $canvasView.scale) / 100)}
+			width="100%"
+			height="100%"
+			fill="url(#pattern-circles)"
+		/>
+
+		<!-- Render the SVGs -->
+		{#each $svgs as svgObj, index}
+			<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+			<!-- svelte-ignore a11y-no-static-element-interactions -->
+			<g
+				transform={`translate(${svgObj.x}, ${svgObj.y})`}
+				on:mouseover={() => {
 				$currentSvgElementIndex = index;
 			}}
-		>
-			{@html svgObj.svg}
-		</g>
-	{/each}
-</svg>
+			>
+				{@html svgObj.svg}
+			</g>
+		{/each}
+	</svg>
+</div>
 
 <style>
 	.draggable {
