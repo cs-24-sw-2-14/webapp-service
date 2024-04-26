fa0ec025d083f0759006ef2452573cdfe4c4b455
Kresten Laust <krestenlaust@gmail.com>
2024-04-26 10:59:43 +0200

    Refactor UserBubbles to use props instead of state (#74)
    * UserBubbles should use props instead of state
    
    Also renamed file
    
    * Add sample data to online users store

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,25 +2,8 @@
 	import Logo from './Logo.svelte';
 	import MenuButton from './MenuButton.svelte';
 	import Icons from '$lib/icons/MenuIcons.json';
-	import OnlineUsers from '$lib/components/OnlineUsers.svelte';
-	import { settings } from '$lib/stores/stateStore';
-
-	// TODO: SHOULD BE A STORE!!!
-	interface OnlineUser {
-		name: string;
-		color: string;
-		border: string;
-	}
-
-	let onlineUsers: OnlineUser[] = [
-		{ name: 'Elma Vukicevic', color: 'bg-blue-500', border: 'border-blue-800' },
-		{ name: 'Amalie Jensen', color: 'bg-red-500', border: 'border-red-800' },
-		{ name: 'Cecilie Lassen', color: 'bg-green-500', border: 'border-green-800' },
-		{ name: 'Kresten Sckerl', color: 'bg-orange-500', border: 'border-orange-800' },
-		{ name: 'Mads Fagerlund', color: 'bg-purple-500', border: 'border-purple-800' },
-		{ name: 'Marc Nygaard', color: 'bg-pink-500', border: 'border-pink-800' },
-		{ name: 'Thorbjørn Larsen', color: 'bg-yellow-500', border: 'border-yellow-800' }
-	];
+	import { settings, onlineUsers } from '$lib/stores/stateStore';
+	import UserBubbles from '$lib/components/UserBubbles.svelte';
 
 	function handleSettingsClick() {
 		settings.set(!$settings);
@@ -39,6 +22,6 @@
 
 	<!-- Who is online? -->
 	<div class="fixed top-0 right-0 z-20 mt-2 mr-14">
-		<OnlineUsers users={onlineUsers} />
+		<UserBubbles users={$onlineUsers} />
 	</div>
 </div>
