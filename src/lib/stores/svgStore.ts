ee983c0c20d4db705d549fcda4c9f925211b43a4
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-05-01 13:43:45 +0200

    simplify svgstore

--- src/lib/stores/svgStore.ts
+++ src/lib/stores/svgStore.ts
@@ -1,57 +1,12 @@
 // svgStore.js
 import { writable } from 'svelte/store';
 
-export enum CoordinateType {
-  moveto,
-  lineto,
-}
-
-export interface PathCoordinate {
-  type: CoordinateType;
+export type Svg = {
+  svg: string;
   x: number;
   y: number;
+  commandId: number;
 }
 
-export interface PlacementCoordinate {
-  x: number;
-  y: number;
-}
+export const svgs = writable<Svg[]>([]);
 
-export interface Drawing {
-  path?: PathCoordinate[];
-  stroke?: string;
-  fill?: string;
-  strokeWidth?: number;
-  placement?: PlacementCoordinate;
-}
-
-export enum svgType {
-  drawing,
-}
-
-export function pathToString(path: PathCoordinate[]) {
-  return path.map((coordinate) => {
-    if (coordinate.type === CoordinateType.moveto) {
-      return `M${coordinate.x},${coordinate.y}`
-    } else {
-      return `L${coordinate.x},${coordinate.y}`
-    }
-  }).join("")
-}
-
-export const drawings = writable<Drawing[]>([]);
-
-export function addDrawing(changes: Drawing) {
-  $drawings = [
-    ...$drawings,
-    changes
-  ]
-}
-
-export function modifyDrawing(index: number, changes: Drawing) {
-  $drawings[index] = changes
-}
-
-export function deleteDrawing(index: number) {
-  $drawings.splice(index, index)
-}
