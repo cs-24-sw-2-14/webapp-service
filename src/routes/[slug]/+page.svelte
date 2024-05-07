14f874c62903e1f0bceaa19825789f0392e1f973
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-05-07 11:35:31 +0200

    set socket endpoint when page is loaded

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import Canvas from '$lib/components/Canvas.svelte';
 	import UI from '$lib/components/UI.svelte';
-	import Websocket from '$lib/websocket.svelte';
-	import { boardId } from '$lib/stores/stateStore';
+	import Websocket from '$lib/components/websocket.svelte';
+	import { boardId, socket } from '$lib/stores/stateStore';
+	import io from 'socket.io-client';
 	export let data;
-
 	boardId.set(data.slug);
+	const ENDPOINT = 'ws://localhost:6123' + '/' + $boardId;
+	socket.set(io(ENDPOINT));
 </script>
 
 <Canvas></Canvas>
