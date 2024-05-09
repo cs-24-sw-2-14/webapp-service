bef2833166777a9c52e87467b26881abe0bf12c4
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-05-09 14:04:18 +0200

    use colormap instead of colortohex and hextocolor and use user and otherusers state

--- src/lib/components/MouseCursors.svelte
+++ src/lib/components/MouseCursors.svelte
@@ -1,41 +1,11 @@
 <script lang="ts">
-	import { colorToHex } from '$lib/color';
+	import { colorMap } from '$lib/color';
 	import { afterUpdate } from 'svelte';
-	import { type CanvasMousePosition, ToolState, type Coordinate } from '$lib/types';
-	import { canvasView, toolState, user } from '$lib/stores/stateStore';
-	import { canvasCursorPosition } from '$lib/stores/stateStore';
+	import { ToolState } from '$lib/types';
+	import { toolState, user, otherUsers } from '$lib/stores/stateStore';
 
-	import { cursors, onlineUsers, chosenColor } from '$lib/stores/stateStore';
-
-	let cursor: Coordinate;
 	let rectElements: any[] = [];
 	let textElements: any[] = [];
-	$: cursor = globalCoordinates($canvasCursorPosition);
-
-	// Update cursors store to include the new local cursor properties
-	$: if ($canvasCursorPosition) {
-		const newLocalCursor = {
-			...$cursors.localCursor,
-			posX: cursor.x,
-			posY: cursor.y,
-			color: $user.color
-		};
-		cursors.update((current) => ({ ...current, localCursor: newLocalCursor }));
-
-		$cursors.localCursor.color = $user.color;
-		$cursors.localCursor.name = $user.name;
-		$user.posX = $cursors.localCursor.posX; // Updating the local user
-		$user.posY = $cursors.localCursor.posY; // Updating the local user
-	}
-
-	// Function to get the position of the cursor in tge "global" coordinate system
-	function globalCoordinates(pos: CanvasMousePosition) {
-		const tx =
-			(pos.x - $canvasView.width / 2) / ($canvasView.scale / 100) + $canvasView.position.x - 10.5;
-		const ty =
-			(pos.y - $canvasView.height / 2) / ($canvasView.scale / 100) + $canvasView.position.y - 11;
-		return { x: tx, y: ty };
-	}
 
 	// Function to dynamically adjust the width of the name label to accommodate the length of the name
 	function adjustBackground() {
@@ -55,19 +25,19 @@
 
 <!-- LOCAL CURSOR (DOSENT HAVE NAME LABEL) -->
 {#if $toolState === ToolState.draw}
-	<g transform={`translate(${$cursors.localCursor.posX}, ${$cursors.localCursor.posY})`}>
-		<circle cx="10" cy="10" r="3" fill={$chosenColor} />
+	<g transform={`translate(${$user.cursorPosition.x - 10.5}, ${$user.cursorPosition.y - 11})`}>
+		<circle cx="10" cy="10" r="3" fill={$user.drawColor} />
 	</g>
 {/if}
 
 <!-- REMOTE CURSORS (OTHER USERS) -->
-{#each $onlineUsers as users, index}
-	<g transform={`translate(${users.posX}, ${users.posY})`}>
+{#each $otherUsers as user, index}
+	<g transform={`translate(${user.cursorPosition.x - 10.5}, ${user.cursorPosition.y - 11})`}>
 		<!-- Remote cursor dot -->
-		<circle cx="10" cy="10" r="3" fill={colorToHex(users.color).secondary} />
+		<circle cx="10" cy="10" r="3" fill={user.drawColor} />
 
 		<!-- Remote name label -->
-		<rect bind:this={rectElements[index]} fill={colorToHex(users.color).secondary} />
+		<rect bind:this={rectElements[index]} fill={colorMap.get(user.color)?.primary} />
 		<text
 			class="select-none"
 			bind:this={textElements[index]}
@@ -75,9 +45,9 @@
 			y="45"
 			font-family="Arial"
 			font-size="20"
-			fill={colorToHex(users.color).primary}
+			fill={colorMap.get(user.color)?.secondary}
 		>
-			{users.name}
+			{user.name}
 		</text>
 	</g>
 {/each}
