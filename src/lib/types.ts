4b57765de844bb79f468c71e41560e0b91aec064
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-05-09 13:51:22 +0200

    format

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -29,22 +29,22 @@ export type UserColor = {
 };
 
 export interface CanvasView {
-  position: Coordinate;
-  width: number; // Default values, will be replaced on mount
-  height: number; // Default values, will be replaced on mount
-  scale: number;
+	position: Coordinate;
+	width: number; // Default values, will be replaced on mount
+	height: number; // Default values, will be replaced on mount
+	scale: number;
 }
 
 export enum ToolState {
-  draw,
-  erase,
-  pan,
-  move
+	draw,
+	erase,
+	pan,
+	move
 }
 
 export interface Coordinate {
-  x: number;
-  y: number;
+	x: number;
+	y: number;
 }
 
 export type CanvasMousePosition = Coordinate;
@@ -55,21 +55,6 @@ export interface Local {
 	posY: number;
 	color: UserColor;
 	name: Username;
-  }
-  
-  export enum UserColor {
-	red,      // 0
-	orange,   // 1
-	yellow,   // 2
-	lime,     // 3
-	green,    // 4
-	teal,     // 5
-	brown,    // 6
-	blue,     // 7
-	purple,   // 8
-	pink      // 9
-  }
-  
   export interface Remote {
 	posX: number;
 	posY: number;
