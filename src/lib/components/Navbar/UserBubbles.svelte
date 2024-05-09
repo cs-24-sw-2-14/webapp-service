ed84e0ca3b4a7332635146b0e19ee7c572aaaa0e
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-05-09 13:57:45 +0200

    use colormap and enums instead

--- src/lib/components/Navbar/UserBubbles.svelte
+++ src/lib/components/Navbar/UserBubbles.svelte
@@ -1,26 +1,24 @@
 <script lang="ts">
-	import { colorToHex } from '$lib/color';
-	import { canvasView } from '$lib/stores/stateStore';
-	import { type User } from '$lib/types';
+	import { colorMap } from '$lib/color';
+	import { canvasView, otherUsers } from '$lib/stores/stateStore';
 	import { getInitials } from '$lib/ts/getInitials';
-
-	export let users: User[];
 </script>
 
 <!-- Display all online users -->
 <div class="relative h-10">
-	{#each users as user, i (user.name)}
+	{#each $otherUsers as user, i (user.name)}
 		<!-- svelte-ignore a11y-click-events-have-key-events -->
 		<!-- svelte-ignore a11y-no-static-element-interactions -->
-		<div
-			on:click={() => {
-				$canvasView.position.x = user.posX;
-				$canvasView.position.y = user.posY;
-			}}
-			class={`w-10 h-10 cursor-pointer select-none rounded-full  text-white text-center text-sm font-mono leading-[2.7em] absolute ring-gray-800 ring-2`}
-			style={`left: ${i * 0.7}em; transform: translateX(${-i * 100}%); background-color: ${colorToHex(user.color).secondary}; border-color: ${colorToHex(user.color).primary}; border-width: 0.2em`}
-		>
-			{getInitials(user.name)}
-		</div>
+		{#if user.isOnline}
+			<div
+				on:click={() => {
+					$canvasView.position = user.cursorPosition;
+				}}
+				class={`w-10 h-10 cursor-pointer select-none rounded-full  text-white text-center text-sm font-mono leading-[2.7em] absolute ring-gray-800 ring-2`}
+				style={`left: ${i * 0.7}em; transform: translateX(${-i * 100}%); background-color: ${colorMap.get(user.color)?.primary}; border-color: ${colorMap.get(user.color)?.secondary}; border-width: 0.2em`}
+			>
+				{getInitials(user.name)}
+			</div>
+		{/if}
 	{/each}
 </div>
