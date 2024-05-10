28d187d2bb2a548cb64d22ee7c5182e1ef83d2ed
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-05-10 08:09:40 +0200

    Create Initilizationpage instead of CustomizationForm

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -2,7 +2,7 @@
 	import Canvas from '$lib/components/Canvas.svelte';
 	import UI from '$lib/components/UI.svelte';
 	import Websocket from '$lib/components/websocket.svelte';
-	import InfoPicker from '$lib/components/CustomizationForm.svelte';
+	import InitilizationPage from '$lib/components/InitilizationPage.svelte';
 	import { boardId, socket, currentPage, Page } from '$lib/stores/stateStore';
 	import io from 'socket.io-client';
 	import { onMount } from 'svelte';
@@ -24,6 +24,6 @@
 	<Canvas />
 	<UI />
 {:else}
-	<InfoPicker />
+	<InitilizationPage />
 {/if}
 <Websocket />
