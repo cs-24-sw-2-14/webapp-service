9a5ddd19fb872f05eb6d21afabcc71219b89204c
MalleGJ <amalie@gabejensen.dk>
2024-05-10 13:32:19 +0200

    Added NaN and numbers as calculations tests to the three functions. 2 functions to go

--- src/lib/sample.spec.ts
+++ src/lib/sample.spec.ts
@@ -60,6 +60,39 @@ describe("", () => {
 
 describe("Center Coordinates In Rectangle", () => {
     test.each([
+        [2/1, 4-2, 2, 2, 1, 1],
+        [0.5*4, 1+1, 2, 2, 1, 1],
+        
+        [2, 2, 2/1, 4-2, 1, 1],
+        [2, 2, 0.5*4, 1+1, 1, 1],
+        
+        [3, 3, 2, 2, 2/1, 4-2],
+        [3, 3, 2, 2, 0.5*4, 1+1],
+
+        [3, 3, 2, 2, 3+2/-2, 3+2/-2],
+
+    ])("Numbers as calculations", (coord1, coord2, rectWidth, rectHeight, expectedCoord1, expectedCoord2) => {
+        expect(centerCoordinatesInRect({ x: coord1, y: coord2 }, { width: rectWidth, height: rectHeight })).toStrictEqual({ x: expectedCoord1, y: expectedCoord2 });
+    });
+    
+    test.each([
+        [NaN, NaN, NaN, NaN, NaN, NaN],
+        [NaN, NaN, 2, 2, NaN, NaN],
+        [1, 1, NaN, NaN, NaN, NaN],
+        [1, 1, 2, NaN, 0, NaN],
+        [1, 1, NaN, 2, NaN, 0],
+        [1, NaN, 2, 2, 0, NaN],
+        [NaN, 1, 2, 2, NaN, 0,],
+
+        [-NaN, -NaN, NaN, NaN, -NaN, -NaN],
+        [-NaN, -NaN, NaN, NaN, NaN, NaN],
+        [NaN, NaN, NaN, NaN, -NaN, -NaN],
+
+    ])("Not a Number", (coord1, coord2, rectWidth, rectHeight, expectedCoord1, expectedCoord2) => {
+        expect(centerCoordinatesInRect({ x: coord1, y: coord2 }, { width: rectWidth, height: rectHeight })).toStrictEqual({ x: expectedCoord1, y: expectedCoord2 });
+    });
+
+    test.each([
         [2, 2, 2, 2, 1, 1],
         [1, 1, 0, 0, 1, 1],
         [1, 2, 2, 2, 0, 1],
@@ -120,17 +153,18 @@ describe("Center Coordinates In Rectangle", () => {
     });
 
     test.each([
-        /*[1.1, 1.1, -1, -1, 0.1, 0.1],
-        [1, 1, -2.5, -2.5, -1.5, -1.5],
-        [1, 1.1, -1.1, -2, -0.1, -0.9],
-        [1.5, 1, -2.5, -1, -1, 0],
+        [1.25, 1.25, -2.5, -2.5, 2.5, 2.5],
+        [2, 1.25, -2.5, -2.5, 3.25, 2.5],
+        [1.25, 2, -2.5, -2.5, 2.5, 3.25],
+        [2.25, 1.25, -4.5, -2.5, 4.5, 2.5],
+        [1.25, 2.25, -2.5, -4.5, 2.5, 4.5],
 
-        [-1.5, -1.5, -1, -1, -2.5, -2.5],
-        [-1, -1, -2.5, -2.5, -3.5, -3.5],
-        [-1, -1.5, -1.5, -2, -2.5, -3.5],
-        [-1.5, -1, -2, -1.5, -3.5, -2.5],
-
-        [10000001.2, 10000001.2, -9000000.6, -9000000.6, 1000000.6, 1000000.6],*/
+        [-1, -2, -1.5, -1.5, -0.25, -1.25],
+        [-1, -2, -1.5, -2.5, -0.25, -0.75],
+        [-1, -2, -2, -1.5, -0, -1.25],
+        [-2, -1, -1.5, -2, -1.25, -0],
+        
+        [1000000.8, 1000000.6, -1000000.4, -1000000.8, 1500001, 1500001],
 
     ])("Negative floats", (coord1, coord2, rectWidth, rectHeight, expectedCoord1, expectedCoord2) => {
         expect(centerCoordinatesInRect({ x: coord1, y: coord2 }, { width: rectWidth, height: rectHeight })).toStrictEqual({ x: expect.closeTo(expectedCoord1), y: expect.closeTo(expectedCoord2) });
@@ -139,6 +173,39 @@ describe("Center Coordinates In Rectangle", () => {
 
 
 describe("Translate Coordinates", () => {
+    test.each([
+        [2/1, 4-2, 2, 2, 4, 4],
+        [0.5*4, 1+1, 2, 2, 4, 4],
+        
+        [2, 2, 2/1, 4-2, 4, 4],
+        [2, 2, 0.5*4, 1+1, 4, 4],
+        
+        [0, 0, 2, 2, 2/1, 4-2],
+        [0, 0, 2, 2, 0.5*4, 1+1],
+
+        [3, 3, 2, 2, 3+2*-2/-2, 3+2*-2/-2],
+
+    ])("Numbers as calculations", (coord1, coord2, offset1, offset2, coord1Offset, coord2Offset) => {
+        expect(translateCoordinates({ x: coord1, y: coord2 }, { x: offset1, y: offset2 })).toStrictEqual({ x: coord1Offset, y: coord2Offset })
+    });
+
+    test.each([
+        [NaN, NaN, NaN, NaN, NaN, NaN],
+        [NaN, NaN, 2, 2, NaN, NaN],
+        [1, 1, NaN, NaN, NaN, NaN],
+        [1, 1, 2, NaN, 3, NaN],
+        [1, 1, NaN, 2, NaN, 3],
+        [1, NaN, 2, 2, 3, NaN],
+        [NaN, 1, 2, 2, NaN, 3,],
+
+        [-NaN, -NaN, NaN, NaN, -NaN, -NaN],
+        [-NaN, -NaN, NaN, NaN, NaN, NaN],
+        [NaN, NaN, NaN, NaN, -NaN, -NaN],
+
+    ])("Not a Number", (coord1, coord2, offset1, offset2, coord1Offset, coord2Offset) => {
+        expect(translateCoordinates({ x: coord1, y: coord2 }, { x: offset1, y: offset2 })).toStrictEqual({ x: coord1Offset, y: coord2Offset })
+    });
+
     test.each([
         [1, 1, 1, 1, 2, 2],
         [1, 1, 0, 0, 1, 1],
@@ -212,6 +279,39 @@ describe("Translate Coordinates", () => {
 
 
 describe("Scale Coordinates", () => {
+    test.each([
+        [2/1, 4-2, 2, 4, 4],
+        [0.5*4, 1+1, 2, 4, 4],
+        
+        [2, 2, 4-2, 4, 4],
+        [2, 2, 2/1, 4, 4],
+        [2, 2, 0.5*4, 4, 4],
+        [2, 2, 1+1, 4, 4],
+        
+        [3, 3, 2, 6/1, 8-2],
+        [3, 3, 2, 0.5*12, 1+5],
+
+        [3, 3, 2, 3/0.5, 3*2],
+
+    ])("Numbers as calculations", (coord1, coord2, scale, coord1Scaled, coord2Scaled) => {
+        expect(scaleCoordinates({ x: coord1, y: coord2 }, scale)).toStrictEqual({ x: coord1Scaled, y: coord2Scaled })
+    });
+
+    test.each([
+        [NaN, NaN, NaN, NaN, NaN],
+        [NaN, NaN, 2, NaN, NaN],
+        [1, 1, NaN, NaN, NaN],
+        [1, NaN, 2, 2, NaN],
+        [NaN, 1, 2, NaN, 2,],
+
+        [-NaN, -NaN, NaN, -NaN, -NaN],
+        [-NaN, -NaN, NaN, NaN, NaN],
+        [NaN, NaN, NaN, -NaN, -NaN],
+
+    ])("Not a Number", (coord1, coord2, scale, coord1Scaled, coord2Scaled) => {
+        expect(scaleCoordinates({ x: coord1, y: coord2 }, scale)).toStrictEqual({ x: coord1Scaled, y: coord2Scaled })
+    });
+
     test.each([
         [1, 1, 2, 2, 2],
         [1, 1, 0, 0, 0],
