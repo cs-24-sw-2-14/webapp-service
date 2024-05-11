9a4854ec11d99b797fec523296521c9cfa3425d0
Kresten Laust <krestenlaust@gmail.com>
2024-05-11 20:14:01 +0200

    Basic text-tool. Adds default text

--- src/lib/components/Tools/TextTool.svelte
+++ src/lib/components/Tools/TextTool.svelte
@@ -1,8 +1,39 @@
-<script>
+<script lang="ts">
 	import MenuButton from '$lib/components/Navbar/MenuButton.svelte';
 	import Icons from '$lib/icons/MenuIcons.json';
-	import { toolState } from '$lib/stores/stateStore';
+	import { canvasTouched, socket, toolState, user } from '$lib/stores/stateStore';
 	import { ToolState } from '$lib/types';
+	import { onMount } from 'svelte';
+	import type { ToolSuccess } from '$lib/types';
+	import { writable } from 'svelte/store';
+
+	let currentCommandId = writable<number | null>(null);
+
+	onMount(() => {
+		if ($socket === undefined) return;
+		$socket.on('startTextSuccess', (data: ToolSuccess) => {
+			if (data.username !== $user.name) return;
+			$currentCommandId = data.commandId;
+		});
+	});
+
+	user.subscribe(startText);
+
+	function startText(){
+		if (!$canvasTouched || $toolState !== ToolState.text || $currentCommandId !== null) return;
+		$socket.emit('startText', {
+			coordinate: $user.position,
+			content: "<empty>",
+			username: $user.name,
+		});
+	}
+	
+	function updateCurrentText(content: string) {
+		$socket.emit('doText', {
+			content: content,
+			commandId: $currentCommandId,
+		});
+	}
 </script>
 
 <MenuButton
