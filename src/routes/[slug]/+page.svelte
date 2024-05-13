1f58674b2b73bb7e6217ab15046d8ec587c5fe75
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-05-13 15:03:36 +0200

    connection to socket at initpage instead

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,24 +1,13 @@
 <script lang="ts">
 	import Canvas from '$lib/components/Canvas.svelte';
 	import UI from '$lib/components/UI.svelte';
-	import Websocket from '$lib/components/Websocket.svelte';
 	import InitializationPage from '$lib/components/InitializationPage.svelte';
-	import { boardId, socket, currentPage } from '$lib/stores/stateStore';
-	import io from 'socket.io-client';
+	import { boardId, currentPage } from '$lib/stores/stateStore';
 
-	import {
-		PUBLIC_SOCKET_API_PROTOCOL,
-		PUBLIC_SOCKET_API_HOSTNAME,
-		PUBLIC_SOCKET_API_PORT
-	} from '$env/static/public';
 	import { Page } from '$lib/types';
 
 	export let data;
 	boardId.set(data.slug);
-
-	const SOCKET_ENDPOINT: string = `${PUBLIC_SOCKET_API_PROTOCOL}://${PUBLIC_SOCKET_API_HOSTNAME}:${PUBLIC_SOCKET_API_PORT}/${$boardId}`;
-	// TODO: Mads, fix socket types
-	socket.set(io(SOCKET_ENDPOINT));
 </script>
 
 {#if $currentPage === Page.CanvasPage}
@@ -27,4 +16,3 @@
 {:else}
 	<InitializationPage />
 {/if}
-<Websocket />
