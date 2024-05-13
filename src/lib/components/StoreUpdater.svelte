e832ce81f1714b45ef8c8109236ed6a67b8926eb
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-05-13 15:03:16 +0200

    rename websocket to storeupdater + refactor

--- src/lib/components/StoreUpdater.svelte
+++ src/lib/components/StoreUpdater.svelte
@@ -2,28 +2,28 @@
 	import { socket } from '$lib/stores/stateStore';
 	import { svgs } from '$lib/stores/svgStore';
 	import { onMount } from 'svelte';
-	import type { Edit, Remove, Svg } from '$lib/types';
+	import type { Svg } from '$lib/types';
+	import type { Edit, Remove } from '$lib/socketioInterface';
 
 	onMount(() => {
 		$socket.on('edit', handleEdit);
 		$socket.on('remove', handleRemove);
 	});
 
-	// TODO: Mads, optionals i interface.
-
 	function handleEdit(data: Edit) {
 		// check if the command already exists
 		const commandIndex = $svgs.findIndex((svg: Svg) => svg.commandId === data.commandId);
 
 		// if it exists, modify it
-		if (commandIndex === -1) {
-			$svgs[commandIndex].placement = data.placement ?? $svgs[commandIndex].placement;
+		if (commandIndex !== -1) {
+			$svgs[commandIndex].offset = data.placement ?? $svgs[commandIndex].offset;
 			$svgs[commandIndex].svgString = data.svgString ?? $svgs[commandIndex].svgString;
 			return;
 		}
 
+		// else add it, if all fields are present
 		if (!data.svgString || !data.placement) return;
-		// else add it
+
 		svgs.update((current) => {
 			let spliceIndex = 0;
 			// Find the index to insert the command into the svgs array
@@ -35,7 +35,7 @@
 			}
 			let svg: Svg = {
 				svgString: data.svgString!,
-				placement: data.placement!,
+				offset: data.placement!,
 				commandId: data.commandId
 			};
 			let resultArray = current;
