b86f55958877ad684d6643486e03c6a3d533cd10
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-05-15 10:46:36 +0200

    connect to initsocket first, then connect to boardsocket

--- src/lib/components/InitializationPage.svelte
+++ src/lib/components/InitializationPage.svelte
@@ -1,32 +1,73 @@
 <script lang="ts">
 	import UserModal from './UserModal.svelte';
+	import {
+		currentPage,
+		username,
+		boardId,
+		color,
+		canvasCursorPosition
+	} from '$lib/stores/stateStore';
+	import { otherUsers, socket, initSocket } from '$lib/stores/socketioStore';
+	import { Page, type Color } from '$lib/types';
 	import { io, Socket } from 'socket.io-client';
+	import { onMount } from 'svelte';
+	import type {
+		ServerToClientEvents,
+		ClientToServerEvents,
+		InitServerToClientEvents
+	} from '$lib/socketioInterface';
 	import {
 		PUBLIC_SOCKET_API_PROTOCOL,
 		PUBLIC_SOCKET_API_HOSTNAME,
 		PUBLIC_SOCKET_API_PORT
 	} from '$env/static/public';
-	import type { ServerToClientEvents, ClientToServerEvents } from '$lib/socketioInterface';
 
-	let dialog: HTMLDialogElement;
-	const SOCKET_ENDPOINT: string = `${PUBLIC_SOCKET_API_PROTOCOL}://${PUBLIC_SOCKET_API_HOSTNAME}:${PUBLIC_SOCKET_API_PORT}/${$boardId}`;
+	const BOARD_SOCKET_ENDPOINT: string = `${PUBLIC_SOCKET_API_PROTOCOL}://${PUBLIC_SOCKET_API_HOSTNAME}:${PUBLIC_SOCKET_API_PORT}/${$boardId}`;
+	const INIT_SOCKET_ENDPOINT: string = `${PUBLIC_SOCKET_API_PROTOCOL}://${PUBLIC_SOCKET_API_HOSTNAME}:${PUBLIC_SOCKET_API_PORT}/${$boardId}_init`;
 
-	function handleSubmit(username: string) {
-		$user.name = username;
-
-		const newSocket: Socket<ServerToClientEvents, ClientToServerEvents> = io(SOCKET_ENDPOINT, {
-			auth: {
-				username: username
-			}
-		});
-
+	onMount(() => {
+		const newSocket: Socket<InitServerToClientEvents> = io(INIT_SOCKET_ENDPOINT);
 		newSocket.on('connect_error', (err) => {
-			console.log(err.message);
+			console.error(err.message);
 		});
 
 		newSocket.on('connect', () => {
-			console.log('Connected!');
+			initSocket.set(newSocket);
+			console.log('connected to init socket');
+		});
+	});
+
+	let dialog: HTMLDialogElement;
+
+	function handleSubmit(username: string, color: Color) {
+		$username = username;
+		$color = color;
+		$initSocket.disconnect();
+
+		const newSocket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
+			BOARD_SOCKET_ENDPOINT,
+			{
+				auth: {
+					username: username,
+					color: color
+				}
+			}
+		);
+
+		newSocket.on('connect_error', (err) => {
+			console.error(err.message);
+		});
+
+		newSocket.on('connect', () => {
 			socket.set(newSocket);
+			console.log('connected to board socket');
+
+			$socket.emit('userChange', {
+				username: $username,
+				color: $color,
+				position: $canvasCursorPosition
+			});
+
 			$currentPage = Page.CanvasPage;
 		});
 	}
@@ -41,6 +82,8 @@
 		submitButtonName="Continue"
 		{handleSubmit}
 		closable={false}
+		otherUsers={$otherUsers}
+		boardId={$boardId}
 	/>
 </main>
 
