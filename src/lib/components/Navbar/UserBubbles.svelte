0e064ef2731d46d1e48644a59304ab8ff82b6594
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-05-15 10:36:26 +0200

    all users in otherUsers are always online

--- src/lib/components/Navbar/UserBubbles.svelte
+++ src/lib/components/Navbar/UserBubbles.svelte
@@ -10,16 +10,14 @@
 	{#each $otherUsers as [_, user], i}
 		<!-- svelte-ignore a11y-click-events-have-key-events -->
 		<!-- svelte-ignore a11y-no-static-element-interactions -->
-		{#if user.isOnline}
-			<div
-				on:click={() => {
-					$canvasView.position = user.position;
-				}}
-				class={`w-10 h-10 cursor-pointer select-none rounded-full  text-white text-center text-sm font-mono leading-[2.7em] absolute ring-gray-800 ring-2`}
-				style={`left: ${i * 0.7}em; transform: translateX(${-i * 100}%); background-color: ${colorMap.get(user.color)?.primary}; border-color: ${colorMap.get(user.color)?.secondary}; border-width: 0.2em`}
-			>
-				{getInitials(user.name)}
-			</div>
-		{/if}
+		<div
+			on:click={() => {
+				$canvasView.position = user.position;
+			}}
+			class={`w-10 h-10 cursor-pointer select-none rounded-full  text-white text-center text-sm font-mono leading-[2.7em] absolute ring-gray-800 ring-2`}
+			style={`left: ${i * 0.7}em; transform: translateX(${-i * 100}%); background-color: ${colorMap.get(user.color)?.primary}; border-color: ${colorMap.get(user.color)?.secondary}; border-width: 0.2em`}
+		>
+			{getInitials(user.name)}
+		</div>
 	{/each}
 </div>
