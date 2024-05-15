100cbfd6c802b4fa4c1f6e6288446d7c4863270e
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-05-15 13:31:11 +0200

    Move socket initilization into socketio store file and expose connect functions

--- src/lib/components/InitializationPage.svelte
+++ src/lib/components/InitializationPage.svelte
@@ -1,76 +1,25 @@
 <script lang="ts">
 	import UserModal from './UserModal.svelte';
-	import {
-		currentPage,
-		username,
-		boardId,
-		color,
-		canvasCursorPosition
-	} from '$lib/stores/stateStore';
-	import { otherUsers, socket, initSocket } from '$lib/stores/socketioStore';
+	import { currentPage, username, boardId, color } from '$lib/stores/stateStore';
+	import { otherUsers, connectToBoardSocket, connectToInitSocket } from '$lib/stores/socketioStore';
 	import { Page, type Color } from '$lib/types';
-	import { io, Socket } from 'socket.io-client';
 	import { onMount } from 'svelte';
-	import type {
-		ServerToClientEvents,
-		ClientToServerEvents,
-		InitServerToClientEvents
-	} from '$lib/socketioInterface';
-	import {
-		PUBLIC_SOCKET_API_PROTOCOL,
-		PUBLIC_SOCKET_API_HOSTNAME,
-		PUBLIC_SOCKET_API_PORT
-	} from '$env/static/public';
-
-	const BOARD_SOCKET_ENDPOINT: string = `${PUBLIC_SOCKET_API_PROTOCOL}://${PUBLIC_SOCKET_API_HOSTNAME}:${PUBLIC_SOCKET_API_PORT}/${$boardId}`;
-	const INIT_SOCKET_ENDPOINT: string = `${PUBLIC_SOCKET_API_PROTOCOL}://${PUBLIC_SOCKET_API_HOSTNAME}:${PUBLIC_SOCKET_API_PORT}/${$boardId}_init`;
+	let dialog: HTMLDialogElement;
 
 	onMount(() => {
-		const newSocket: Socket<InitServerToClientEvents> = io(INIT_SOCKET_ENDPOINT);
-		newSocket.on('connect_error', (err) => {
-			console.error(err.message);
-		});
-
-		newSocket.on('connect', () => {
-			initSocket.set(newSocket);
+		connectToInitSocket($boardId, () => {
 			console.log('connected to init socket');
 		});
 	});
 
-	let dialog: HTMLDialogElement;
-
 	function handleSubmit(username: string, color: Color) {
-		$username = username;
-		$color = color;
-		$initSocket.disconnect();
-
-		const newSocket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
-			BOARD_SOCKET_ENDPOINT,
-			{
-				auth: {
-					username: username,
-					color: color
-				}
-			}
-		);
-
-		newSocket.on('connect_error', (err) => {
-			console.error(err.message);
-		});
-
-		newSocket.on('connect', () => {
-			socket.set(newSocket);
-			console.log('connected to board socket');
-
-			$socket.emit('userChange', {
-				username: $username,
-				color: $color,
-				position: $canvasCursorPosition
-			});
-
+		connectToBoardSocket(username, color, $boardId, () => {
+			$username = username;
+			$color = color;
 			$currentPage = Page.CanvasPage;
 		});
 	}
+
 	$: if (dialog) dialog.showModal();
 </script>
 
--- src/lib/stores/socketioStore.ts
+++ src/lib/stores/socketioStore.ts
@@ -1,6 +1,6 @@
-import type { CommandId, Svg, User, Username } from '$lib/types';
+import type { BoardId, Color, CommandId, Svg, User, Username } from '$lib/types';
 import { writable, readable, get } from 'svelte/store';
-import { Socket } from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 import type {
 	ClientToServerEvents,
 	ServerToClientEvents,
@@ -12,37 +12,67 @@ import type {
 } from '$lib/socketioInterface';
 import { username } from './stateStore';
 
-export const socket = writable<Socket<ServerToClientEvents, ClientToServerEvents>>();
-export const initSocket = writable<Socket<InitServerToClientEvents>>();
+import {
+	PUBLIC_SOCKET_API_PROTOCOL,
+	PUBLIC_SOCKET_API_HOSTNAME,
+	PUBLIC_SOCKET_API_PORT
+} from '$env/static/public';
+
+const SOCKET_ENDPOINT: string = `${PUBLIC_SOCKET_API_PROTOCOL}://${PUBLIC_SOCKET_API_HOSTNAME}:${PUBLIC_SOCKET_API_PORT}`;
+
+export const initSocket = writable<Socket<InitServerToClientEvents>>(io(SOCKET_ENDPOINT));
+export const boardSocket = writable<Socket<ServerToClientEvents, ClientToServerEvents>>(
+	io(SOCKET_ENDPOINT)
+);
+
+export function connectToBoardSocket(
+	username: Username,
+	color: Color,
+	boardId: BoardId,
+	connectCallback: () => void
+) {
+	boardSocket.set(
+		io(SOCKET_ENDPOINT + `/${boardId}`, {
+			auth: {
+				username: username,
+				color: color
+			}
+		})
+	);
+	get(boardSocket).on('connect', () => {
+		connectCallback();
+	});
+}
+
+export function connectToInitSocket(boardId: BoardId, connectCallback: () => void) {
+	initSocket.set(io(SOCKET_ENDPOINT + `/${boardId}_init`));
+	get(initSocket).on('connect', () => {
+		connectCallback();
+	});
+}
 
 export const otherUsers = readable<Map<Username, User>>(new Map(), (set) => {
-	if (get(socket)) {
-		get(socket).on('userChange', (data) => {
-			handleUserChange(data, get(otherUsers), get(username), set);
-		});
-		get(socket).on('userRemove', (data) => {
-			handleUserRemove(data, get(otherUsers), set);
-		});
-	}
-	if (get(initSocket)) {
-		get(initSocket).on('userChange', (data) => {
-			handleUserChange(data, get(otherUsers), get(username), set);
-		});
-		get(initSocket).on('userRemove', (data) => {
-			handleUserRemove(data, get(otherUsers), set);
-		});
-	}
+	get(boardSocket).on('userChange', (data) => {
+		handleUserChange(data, get(otherUsers), get(username), set);
+	});
+	get(boardSocket).on('userRemove', (data) => {
+		handleUserRemove(data, get(otherUsers), set);
+	});
+	get(initSocket).on('userChange', (data) => {
+		handleUserChange(data, get(otherUsers), get(username), set);
+	});
+	get(initSocket).on('userRemove', (data) => {
+		handleUserRemove(data, get(otherUsers), set);
+	});
 });
 
 export const svgs = writable<Map<CommandId, Svg>>(new Map(), (set) => {
-	if (get(socket)) {
-		get(socket).on('edit', (data) => {
-			handleEdit(data, get(svgs), set);
-		});
-		get(socket).on('remove', (data) => {
-			handleRemove(data, get(svgs), set);
-		});
-	}
+	get(boardSocket).on('edit', (data) => {
+		handleEdit(data, get(svgs), set);
+	});
+	get(boardSocket).on('remove', (data) => {
+		handleRemove(data, get(svgs), set);
+	});
 });
 
 function handleUserChange(
@@ -75,6 +105,7 @@ function handleEdit(
 	svgs: Map<CommandId, Svg>,
 	set: (svgs: Map<CommandId, Svg>) => void
 ) {
+	console.log('edit', data);
 	const oldSvg = svgs.get(data.commandId)!;
 	svgs.set(data.commandId, {
 		commandId: data.commandId,
@@ -90,6 +121,7 @@ function handleRemove(
 	svgs: Map<CommandId, Svg>,
 	set: (svgs: Map<CommandId, Svg>) => void
 ) {
+	console.log('remove', data);
 	if (!svgs.has(data.commandId)) return;
 	const oldSvg = svgs.get(data.commandId)!;
 	svgs.set(data.commandId, {
