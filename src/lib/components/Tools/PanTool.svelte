6845296fab49c3b283b91331d430706275921fcd
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-05-15 10:41:20 +0200

    use new stores

--- src/lib/components/Tools/PanTool.svelte
+++ src/lib/components/Tools/PanTool.svelte
@@ -1,31 +1,27 @@
 <script lang="ts">
 	import MenuButton from '$lib/components/Navbar/MenuButton.svelte';
 	import Icons from '$lib/icons/MenuIcons.json';
-	import {
-		toolState,
-		canvasView,
-		cursorPosition,
-		canvasTouched
-	} from '$lib/stores/stateStore';
+	import { chosenTool, canvasView, cursorPosition, cursorDown } from '$lib/stores/stateStore';
 	import { type ViewportCoordinateSet, ToolState } from '$lib/types';
 	let startX: number, startY: number;
 
-	canvasTouched.subscribe(startDrag);
+	cursorDown.subscribe(startDrag);
 	cursorPosition.subscribe(doDrag);
 
 	function startDrag(isDown: boolean) {
-		if ($toolState !== ToolState.pan) return;
+		if ($chosenTool !== ToolState.pan) return;
 		if (isDown) {
 			startX = $cursorPosition.x;
 			startY = $cursorPosition.y;
 		}
 	}
 
-		if (!$canvasTouched || $toolState !== ToolState.pan) return;
 	function doDrag(pos: ViewportCoordinateSet) {
+		if (!$cursorDown || $chosenTool !== ToolState.pan) return;
 		$canvasView = {
 			...$canvasView,
-			position: { // TODO: reimplment PanTool
+			position: {
+				// TODO: reimplment PanTool
 				x: $canvasView.position.x - (pos.x - startX) / ($canvasView.scale / 100),
 				y: $canvasView.position.y - (pos.y - startY) / ($canvasView.scale / 100)
 			}
@@ -36,9 +32,9 @@
 </script>
 
 <MenuButton
-	isActive={$toolState === ToolState.pan}
+	isActive={$chosenTool === ToolState.pan}
 	icon={Icons.pan}
 	on:click={() => {
-		$toolState = ToolState.pan;
+		$chosenTool = ToolState.pan;
 	}}
 ></MenuButton>
