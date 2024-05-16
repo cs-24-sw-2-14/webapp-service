88260e8292f2af4b2699faef15098b0a6f484c23
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-05-16 11:22:04 +0200

    remove unintended charaacter

--- src/lib/components/Tools/PanTool.svelte
+++ src/lib/components/Tools/PanTool.svelte
@@ -1,25 +1,25 @@
 <script lang="ts">
 	import MenuButton from '$lib/components/Navbar/MenuButton.svelte';
 	import Icons from '$lib/icons/MenuIcons.json';
-	import { chosenTool, canvasView, <canvasCursorPosition, cursorDown } from '$lib/stores/stateStore';
+	import { chosenTool, canvasView, canvasCursorPosition, cursorDown } from '$lib/stores/stateStore';
 	import { type CanvasCoordinateSet, ToolState } from '$lib/types';
-  let isDragging = false;
-  let startPosition: CanvasCoordinateSet
+	let isDragging = false;
+	let startPosition: CanvasCoordinateSet;
 
 	cursorDown.subscribe(startDrag);
 	canvasCursorPosition.subscribe(doDrag);
 
 	function startDrag() {
 		if ($chosenTool !== ToolState.pan || !$cursorDown) return;
-    startPosition = $canvasCursorPosition
-    isDragging = true;
+		startPosition = $canvasCursorPosition;
+		isDragging = true;
 	}
 
 	function doDrag() {
 		if (!$cursorDown || $chosenTool !== ToolState.pan) {
-      isDragging = false;
-      return;
-    } 
+			isDragging = false;
+			return;
+		}
 
 		$canvasView = {
 			...$canvasView,
@@ -29,11 +29,8 @@
 			}
 		};
 
-    startPosition = $canvasCursorPosition
+		startPosition = $canvasCursorPosition;
 	}
-
-
-
 </script>
 
 <MenuButton
