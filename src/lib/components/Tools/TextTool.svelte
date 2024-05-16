1d81f8eeaa77d51ee966a4757c6a5627945a40dc
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-05-16 10:01:06 +0200

    change some of the stores

--- src/lib/components/Tools/TextTool.svelte
+++ src/lib/components/Tools/TextTool.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import MenuButton from '$lib/components/Navbar/MenuButton.svelte';
 	import Icons from '$lib/icons/MenuIcons.json';
-	import { canvasTouched, socket, toolState, user } from '$lib/stores/stateStore';
+	import { cursorDown, chosenTool, username, canvasCursorPosition } from '$lib/stores/stateStore';
+	import { boardSocket } from '$lib/stores/socketioStore';
 	import { ToolState } from '$lib/types';
 	import { onMount } from 'svelte';
 	import type { ToolSuccess } from '$lib/types';
@@ -10,39 +11,37 @@
 	let currentCommandId = writable<number | null>(null);
 
 	onMount(() => {
-		if ($socket === undefined) return;
-		$socket.on('startTextSuccess', (data: ToolSuccess) => {
-			if (data.username !== $user.name) return;
+		if ($boardSocket === undefined) return;
+		$boardSocket.on('startTextSuccess', (data: ToolSuccess) => {
+			if (data.username !== $username) return;
 			$currentCommandId = data.commandId;
-
-			// Testing
-			updateCurrentText("TEST");
+			updateCurrentText('TEST');
 		});
 	});
 
-	user.subscribe(startText);
-	canvasTouched.subscribe(startText);
+	canvasCursorPosition.subscribe(startText);
+	cursorDown.subscribe(startText);
 
-	function startText(){
-		if (!$canvasTouched || $toolState !== ToolState.text || $currentCommandId !== null) return;
-		$socket.emit('startText', {
+	function startText() {
+		if (!$cursorDown || $chosenTool !== ToolState.text || $currentCommandId !== null) return;
+		$boardSocket.emit('startText', {
 			placement: $user.position,
-			username: $user.name,
+			username: $user.name
 		});
 	}
-	
+
 	function updateCurrentText(content: string) {
-		$socket.emit('doText', {
+		$boardSocket.emit('doText', {
 			content: content,
-			commandId: $currentCommandId,
+			commandId: $currentCommandId
 		});
 	}
 </script>
 
 <MenuButton
-	isActive={$toolState === ToolState.text}
+	isActive={$chosenTool === ToolState.text}
 	icon={Icons.text}
 	on:click={() => {
-		$toolState = ToolState.text;
+		$chosenTool = ToolState.text;
 	}}
 ></MenuButton>
